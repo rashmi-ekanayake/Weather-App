d2eb318a79f75dee781c2353623daf2208b2bc85
RashmiAyodhya <jennie20.23rak@gmail.com>
2025-06-11 04:39:22 +0530

    Set up main window and background color

---
+++ Weather_App.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import tkinter as tk
+
+
+root = Tk()
+root.title("Weather App")
+root.geometry("900x600+300+100")
+root.resizable(True, False)  
+root.configure(bg="#f5f7f")   
+
+
+BG_COLOR = "#f5f7f"            
+CARD_COLOR = "#ffffff"
+ACCENT_COLOR = "#3a7bd"        
+TEXT_COLOR = "#333333"
+SECONDARY_COLOR = "#66666"     
